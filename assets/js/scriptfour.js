ebc88f3ff51ea6bc95151c3144fed4bd2b1a63d2
Muhitul Islam <imuhitul83@gmail.com>
2025-07-13 16:11:25 +0600

    Add files via upload

---
+++ assets/js/scriptfour.js
@@ -0,0 +1,6 @@
+//pricing hover remove
+
+let pricingBox = document.querySelector('.single_background');
+pricingBox.addEventListener('mouseover', () => {
+    pricingBox.classList.remove('single_background');
+});
---
+++ assets/js/scriptseven.js
@@ -0,0 +1,6 @@
+let closesideBtn = document.querySelector('.sidebarClose');
+let removeclassarea = document.querySelector('.sidebar');
+
+closesideBtn.addEventListener('click', () => {
+    removeclassarea.classList.remove('sidebarshow');
+});
